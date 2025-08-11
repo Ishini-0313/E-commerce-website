a0a42c26b725732baf9ac9cbb21fbf3dbeb3bcdf
Ishini <misnfernando1@gmail.com>
2025-08-11 22:07:52 +0530

    insert category button coded

---
+++ admin_area/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    
+    <!-- css file -->
+     <link rel="stylesheet" href="../style.css">
+    
+     <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        .footer{
+            position: absolute; /*relative to the body */
+            bottom: 0;
+        }
+    </style>
+</head>
+<body>
+    <!-- navbar -->
+     <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../images/pngtree-colorful-shopping-cart-logo-vector-png-image_6969977.png" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Welcome guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <!-- second child -->
+         <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+         </div>
+
+         <!-- third child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="p-3">
+                    <a href="#">
+                        <img src="../images/pineapplejuice.jpg" alt="" class="admin_img">
+                    </a>
+                    <p class="text-light text-center">Admin name</p>
+                </div>
+                <div class="button text-center">
+                    <!-- button*10>a.nav-link.text-light.bg-info.my-1 -->
+                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
+                    <button><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Viewt Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+                </div>
+            </div>
+        </div>
+
+        <!-- fourth child -->
+        <div class="container my-5">
+            <?php
+                if(isset($_GET['insert_category'])){
+                    include('insert_categories.php');
+                }
+            ?>
+        </div>
+    </div>
+
+    <div class="bg-info p-3 text-center footer">
+            <p>All rights reserved &copy Designed by Ishini-2025</p>
+         </div>
+
+    <!-- bootstrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
