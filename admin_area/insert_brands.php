55d140307ff144600a7ce9fc200a319bf90aa3e1
Ishini <misnfernando1@gmail.com>
2025-08-12 23:53:00 +0530

    ...

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,21 +1,21 @@
 <?php
     include('../includes/connect.php');
 
-    if(isset($_POST['insert_cat'])){
-        $category_title = $_POST['cat_title'];
+    if(isset($_POST['insert_brand'])){
+        $brand_title = $_POST['brand_title'];
 
         //select data from database
-        $select_qry = "SELECT * FROM categories WHERE category_title = '$category_title' ";
+        $select_qry = "SELECT * FROM brands WHERE brand_title = '$brand_title' ";
         $result_select = mysqli_query($con ,$select_qry );
         $no = mysqli_num_rows($result_select);
         if($no>0){
-            echo "<script>alert('This category is present in the database')</script>";
+            echo "<script>alert('This brand is present in the database')</script>";
         }
         else{
-            $insert_qry = "INSERT INTO categories (category_title) VALUES ('$category_title')";
+            $insert_qry = "INSERT INTO brands (brand_title) VALUES ('$brand_title')";
             $result = mysqli_query($con ,$insert_qry );
             if($result){
-                echo "<script>alert('Category has been inserted successfully')</script>";
+                echo "<script>alert('Brand has been inserted successfully')</script>";
             }
         }
         
@@ -26,11 +26,11 @@
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-        <input type="text" class="form-control" name="cat_title" placeholder="Insert brands" aria-label="Brands" aria-describedby="basic-addon1">
+        <input type="text" class="form-control" name="brand_title" placeholder="Insert brands" aria-label="Brands" aria-describedby="basic-addon1">
     </div>
 
     <div class="input-group w-10 mb-2 m-auto">
-        <input type="submit" class=" bg-info" name="insert_cat" value="Insert Brands">
+        <input type="submit" class=" bg-info border-0 p-2 my-3" name="insert_brand" value="Insert Brands">
          
     </div>
 </form>
