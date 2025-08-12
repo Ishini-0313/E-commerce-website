c13d19886d02c1c509416af43827b836b13bccd2
Ishini <misnfernando1@gmail.com>
2025-08-12 23:30:19 +0530

    ..

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,5 +1,16 @@
 <?php
     include('../includes/connect.php');
+
+    //select data from database
+    
+    if(isset($_POST['insert_cat'])){
+        $category_title = $_POST['cat_title'];
+        $insert_qry = "INSERT INTO categories (category_title) VALUES ('$category_title')";
+        $result = mysqli_query($con ,$insert_qry );
+        if($result){
+            echo "<script>alert('Category has been inserted successfully')</script>";
+        }
+    }
 ?>
 
 <form action="" method="post" class="mb-2">
@@ -9,7 +20,6 @@
     </div>
 
     <div class="input-group w-10 mb-2 m-auto">
-        <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Categories"> -->
-         <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
+        <input type="submit" class="bg-info border-0 p-2 my-3" name="insert_cat" value="Insert Categories">
     </div>
 </form>
