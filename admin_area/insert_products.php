1c14813560e28d559d6407d03e9e79acdc5e9511
Ishini <misnfernando1@gmail.com>
2025-08-13 22:31:38 +0530

    ..

---
+++ admin_area/insert_products.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Products-Admin Dashboard</title>
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    
+    <!-- css file -->
+     <link rel="stylesheet" href="../style.css">
+    
+     <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body class="bg-light">
+    <div class="container mt-3">
+        <h1 class="text-center">Insert Products</h1>
+        <!-- form -->
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="form-outline mb-4">
+                <label for="product_label" class="form-label"></label>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
