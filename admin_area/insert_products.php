56efdec0f4a4b1c912aee294bbcf9996161ffa20
Ishini <misnfernando1@gmail.com>
2025-08-15 20:14:50 +0530

    insert product completed

--- admin_area/insert_products.php
+++ admin_area/insert_products.php
@@ -1,3 +1,7 @@
+<?php
+    include("../includes/connect.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,10 +46,16 @@
             <div class="form-outline mb-4 w-50 m-auto">
                 <select name="product_category" class="form-select" id="">
                     <option value="">Select a category</option>
-                    <option value="">Select a category</option>
-                    <option value="">Select a category</option>
-                    <option value="">Select a category</option>
-                    <option value="">Select a category</option>
+
+                    <?php
+                        $select_qry = "SELECT * FROM categories";
+                        $result = mysqli_query($con , $select_qry);
+                        while($row = mysqli_fetch_assoc($result)){
+                            $category_title = $row['category_title'];
+                            $category_id = $row['category_id'];
+                            echo "<option value='$category_id'>$category_title</option>";
+                        }
+                    ?>
                 </select>
             </div>
 
@@ -53,10 +63,16 @@
             <div class="form-outline mb-4 w-50 m-auto">
                 <select name="product_brand" class="form-select" id="">
                     <option value="">Select a brand</option>
-                    <option value="">Select a category</option>
-                    <option value="">Select a category</option>
-                    <option value="">Select a category</option>
-                    <option value="">Select a category</option>
+                    <?php
+                        $select_qry = "SELECT * FROM brands";
+                        $result = mysqli_query($con , $select_qry);
+                        
+                        while($row = mysqli_fetch_assoc($result)){
+                            $brand_title = $row['brand_title'];
+                            $brand_id = $row['brand_id'];
+                            echo "<option value='$brand_id'>$brand_title</option>";
+                        }
+                    ?> 
                 </select>
             </div>
 
@@ -82,6 +98,11 @@
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_price" class="form-label">Product Price</label>
                 <input type="text" name="product_price" id="product_price" class="form-control" placeholder="Enter product price" autocomplete="off" required>
+            </div>
+
+            <!-- submit btn -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <input type="submit" name="insert_product"  class="btn btn-info mb-3 px-3" value="Insert Product">
             </div>
         </form>
     </div>
