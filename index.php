1f9d6be212ceb8f0d48fa363e8ff7f5f5d0b3cc5
Ishini <misnfernando1@gmail.com>
2025-08-16 22:44:35 +0530

    ....

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
         </div>
 
         <!-- fourth child -->
-        <div class="row">
+        <div class="row mx-2">
             <div class="col-md-10">
                 <!-- products -->
                 <div class="row">
@@ -101,10 +101,10 @@
                             echo "
                                 <div class='col-md-4 mb-2'>
                                     <div class='card'>
-                                        <img src=$img1 class='card-img-top' alt='...'>
+                                        <img src='./admin_area/product_images/$img1' class='card-img-top' alt='$title '>
                                         <div class='card-body'>
-                                            <h5 class='card-title'>Card title</h5>
-                                            <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                            <h5 class='card-title'>$title</h5>
+                                            <p class='card-text'>$desc</p>
                                             <a href='#' class='btn btn-info'>Add to cart</a>
                                             <a href='#' class='btn btn-secondary'>View More</a>
                                         </div>
