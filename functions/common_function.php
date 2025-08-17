287f3ecad7f1cdc648c0a8bf14f34b358ac0def6
Ishini <misnfernando1@gmail.com>
2025-08-17 10:52:27 +0530

    getCategories function coded

--- functions/common_function.php
+++ functions/common_function.php
@@ -47,4 +47,17 @@
                         }
     }
 
+    //displaying categories inside nav
+    function getCategories(){
+        global $con;
+        $select_categories = "SELECT * FROM categories";
+                        $result_categories = mysqli_query($con , $select_categories);
+                        while($row_data = mysqli_fetch_assoc($result_categories)){
+                            $category_title = $row_data['category_title'];
+                            $category_id = $row_data['category_id'];
+                            echo "<li class='nav-item '>
+                                    <a href='index.php?category=$category_id' class='nav-link text-light'>$category_title</a>
+                                </li>";
+                        }
+    }
 ?>
