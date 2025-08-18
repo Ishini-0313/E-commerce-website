4d3af41f5b1753702f2e7967648ba3e56584d950
Ishini <misnfernando1@gmail.com>
2025-08-18 17:14:36 +0530

    get_unique_categories func coded

--- functions/common_function.php
+++ functions/common_function.php
@@ -39,6 +39,43 @@
         }
     }
 
+    //getting unique categories
+    function get_unique_categories(){
+        global $con;
+
+        //condition to check isset or not
+        if(isset($_GET['category'])){
+            $category_id = $_GET['category'];
+                $select_qry = "SELECT * FROM products WHERE category_id = $category_id";
+                        $result = mysqli_query($con , $select_qry);
+                        while($row = mysqli_fetch_assoc($result)){
+                            $id = $row['product_id'];
+                            $title = $row['product_title'];
+                            $desc = $row['product_description'];
+                            //$keywords = $row['product_keywords'];
+                            $img1 = $row['product_image1'];
+                            $price = $row['product_price'];
+                            $cat_id = $row['category_id'];
+                            $br_id = $row['brand_id'];
+
+                            echo "
+                                <div class='col-md-4 mb-2'>
+                                    <div class='card'>
+                                        <img src='./admin_area/product_images/$img1' class='card-img-top' alt='$title '>
+                                        <div class='card-body'>
+                                            <h5 class='card-title'>$title</h5>
+                                            <p class='card-text'>$desc</p>
+                                            <a href='#' class='btn btn-info'>Add to cart</a>
+                                            <a href='#' class='btn btn-secondary'>View More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            ";
+                        }
+            
+        }
+    }
+
     //displaying brands inside nav
     function getBrands(){
         global $con;
