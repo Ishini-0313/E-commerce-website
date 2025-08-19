c8c9ccb1c260c464ff47bcfb3bd5d156fcd1f3ec
Ishini <misnfernando1@gmail.com>
2025-08-19 22:16:11 +0530

    created footer file inside includes folder

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="bg-info p-3 text-center">
+            <p>All rights reserved &copy Designed by Ishini-2025</p>
+</div>
--- index.php
+++ index.php
@@ -122,9 +122,10 @@
 
 
         <!-- last child -->
-        <div class="bg-info p-3 text-center">
-            <p>All rights reserved &copy Designed by Ishini-2025</p>
-         </div>
+        <!-- include footer -->
+        <?php
+            include('includes/footer.php');
+        ?>
     </div>
 
     <!-- bootstrap js link -->
