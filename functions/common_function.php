b4a0be87ce09fb9c3787b1bed9d93dac0ba62336
Ishini <misnfernando1@gmail.com>
2025-08-20 19:40:55 +0530

    view_details func coded

--- functions/common_function.php
+++ functions/common_function.php
@@ -5,7 +5,6 @@
     //getting products
     function getProducts(){
         global $con;
-
         //condition to check isset or not
         if(!isset($_GET['category'])){
             if(!isset($_GET['brand'])){
@@ -228,5 +227,42 @@
         
     }
         
-    
+    //view details function
+    function view_details(){
+        global $con;
+        //condition to check isset or not
+        if(isset($_GET['product_id'])){
+            if(!isset($_GET['category'])){
+                if(!isset($_GET['brand'])){
+                    $product_id = $_GET['product_id'];
+                    $select_qry = "SELECT * FROM products order by rand() LIMIT 0,3";
+                            $result = mysqli_query($con , $select_qry);
+                            while($row = mysqli_fetch_assoc($result)){
+                                $id = $row['product_id'];
+                                $title = $row['product_title'];
+                                $desc = $row['product_description'];
+                                //$keywords = $row['product_keywords'];
+                                $img1 = $row['product_image1'];
+                                $price = $row['product_price'];
+                                $cat_id = $row['category_id'];
+                                $br_id = $row['brand_id'];
+
+                                echo "
+                                    <div class='col-md-4 mb-2'>
+                                        <div class='card'>
+                                            <img src='./admin_area/product_images/$img1' class='card-img-top' alt='$title '>
+                                            <div class='card-body'>
+                                                <h5 class='card-title'>$title</h5>
+                                                <p class='card-text'>$desc</p>
+                                                <a href='#' class='btn btn-info'>Add to cart</a>
+                                                <a href='product_details.php?product_id=$id' class='btn btn-secondary'>View More</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ";
+                            }
+                }
+            }
+        }
+    }
 ?>
