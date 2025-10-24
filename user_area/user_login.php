384cb569cd9687ce5effa6032a9f8f73f0fe6279
Ishini <misnfernando1@gmail.com>
2025-10-24 21:50:41 +0530

    created

--- user_area/user_login.php
+++ user_area/user_login.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User Login</title>
+    <!--  bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
+    <!-- font awsome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- css file -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>User login</h1>
-</body>
+    <div class="container-fluid my-3">
+        <h2 class="text-center">
+            User Login
+        </h2>
+        <div class="row d-flex align-items-center justify-content-center mt-5">
+            <div class="col-lg-12 col-xl-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <!-- username field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_name" class="form-label">Username</label>
+                        <input type="text" id="user_name" class="form-control" placeholder="Enter your username" autocomplete="off" required="required" name="user_name">
+                    </div>
+                    <!-- password field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_pwd" class="form-label">Password</label>
+                        <input type="password" id="user_pwd" class="form-control" placeholder="Enter your password" autocomplete="off" required="required" name="user_pwd">
+                    </div>
+                    <!-- login btn -->
+                    <div class="mt-4 pt-2">
+                        <input type="submit" value="Login" class="bg-info border-0 py-2 px-3" name="user_login">
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account ? <a href="user_registration.php" class="text-danger"> Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
 </html>
