45b1fc7fd771f14adbfb4c1ad4f5115cd19bd696
Ishini <misnfernando1@gmail.com>
2025-11-14 23:58:26 +0530

    delete items from cart

--- user_area/order.php
+++ user_area/order.php
@@ -49,5 +49,8 @@
     $insert_pending_orders = "INSERT INTO orders_pending (User_id,invoice_number,product_id,quantity,order_status) VALUES ($user_id,$invoice_no,$product_id,$quantity,'$status')";
     $result_pending_orders = mysqli_query($con, $insert_pending_orders);
     
+    //delete items from cart
+    $empty_cart = "DELETE FROM cart_details where ip_address='$get_ip_address'";
+    $result_delete = mysqli_query($con, $empty_cart);
 
 ?>
